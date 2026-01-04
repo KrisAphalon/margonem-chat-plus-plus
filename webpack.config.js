3f647f8505179fe161c03e9b40c8697139401310
Krzysztof Kaczmarzyk <k.kaczmarzyk@interia.pl>
2026-01-04 23:10:37 +0100

    refactor: use node prefix for node imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-import * as childProcess from "child_process";
-import * as fs from "fs";
-import * as path from "path";
+import * as childProcess from "node:child_process";
+import * as fs from "node:fs";
+import * as path from "node:path";
 import * as sass from "sass";
 import webpack from "webpack";
 
